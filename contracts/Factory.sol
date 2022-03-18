92cba60f93e8c964c9f123097a6bb59a70fbe10e
Christian Dahdah <christiandahdah@gmail.com>
2022-03-18 16:32:12 +0100

    fix(pause, #3): added explicitly pause interface to factory

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -18,8 +18,9 @@ pragma solidity =0.8.12;
 
 import "./Pool.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./interfaces/IPausedFactory.sol";
 
-contract Factory {
+contract Factory is IPausedFactory {
 
     using SafeERC20 for IERC20; 
 
