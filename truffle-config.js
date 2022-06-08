659104247b493c326c9be2e79f6401a974d4df21
David Bouba <d.m.bouba@gmail.com>
2022-06-08 17:55:47 +0200

    clean: updated truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -10,62 +10,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
-      gasPrice: 20000000000,
-    },
-    kovan: {
-      provider: function() {
-        return new HDWalletProvider(
-          //private keys array
-          process.env.MNEMONIC,
-          //url to ethereum node
-          process.env.INFURA_KOVAN_ENDPOINT_URL,
-          //account index
-          process.env.ACCOUNT_INDEX
-        );
-      },
-//      gas: 12487794,
-      gasPrice: 6000000000,
-      network_id: 42,
-      timeoutBlocks: 1000,
-      skipDryRun: true,
-      timeoutBlocks: 50000,
-      networkCheckTimeout: 1000000
-    },
-	rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(
-          //private keys array
-          process.env.MNEMONIC,
-          //url to ethereum node
-          process.env.INFURA_RINKEBY_ENDPOINT_URL,
-          //account index
-          process.env.ACCOUNT_INDEX
-        );
-      },
-      gasPrice: 2000000000,
-      network_id: 4,
-      timeoutBlocks: 1000,
-      skipDryRun: true,
-      timeoutBlocks: 50000,
-      networkCheckTimeout: 1000000
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          //private keys array
-          process.env.MNEMONIC,
-          //url to ethereum node
-          process.env.INFURA_ROPSTEN_ENDPOINT_URL,
-          //account index
-          process.env.ACCOUNT_INDEX
-        );
-      },
-      gasPrice: 40000000000,
-      network_id: 3,
-      timeoutBlocks: 1000,
-      skipDryRun: true,
-      timeoutBlocks: 50000,
-      networkCheckTimeout: 1000000
     },
     polygon: {
       provider: () => new HDWalletProvider(
@@ -90,7 +34,7 @@ module.exports = {
           //private keys array
           process.env.MNEMONIC,
           //url to polygon node
-          process.env.INFURA_MUMBAI_ENDPOINT_URL,
+          "https://matic-mumbai.chainstacklabs.com",
           //account index
           process.env.ACCOUNT_INDEX
         );
@@ -100,49 +44,19 @@ module.exports = {
       confirmations: 2,
       timeoutBlocks: 1000,
       skipDryRun: true,
-//      websocket: true,
       timeoutBlocks: 50000,
       networkCheckTimeout: 1000000
-    },
-	bscTestnet: {
-      provider: function() {
-        return new HDWalletProvider(
-          //private keys array
-          process.env.MNEMONIC,
-          //url to bsc node
-          "https://data-seed-prebsc-1-s1.binance.org:8545",
-          //account index
-          process.env.ACCOUNT_INDEX
-        );
-      },
-      network_id: 97,
-      timeoutBlocks: 200,
-      skipDryRun: true,
-    },
-    harmonyTestnet: {
-      provider: () => {
-		return new HDWalletProvider(
-          //private keys array
-          process.env.MNEMONIC,
-          //url to harmony node
-          "https://api.s0.b.hmny.io",
-          //account index
-          process.env.ACCOUNT_INDEX
-        );
-      },
-      network_id: 1666700000,
     },
   },
   compilers: {
     solc: {
       version: "0.8.12",
       settings: {
-        // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 1
+          runs: 100
         },
-        evmVersion: "istanbul",
+        evmVersion: "london",
         outputSelection: {
           "*": {
             "": ["ast"],
