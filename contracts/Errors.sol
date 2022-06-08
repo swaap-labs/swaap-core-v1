f326dd10ce80ad050958f9a891aff47a792ba713
David Bouba <d.m.bouba@gmail.com>
2022-06-08 18:37:24 +0200

    feat(Errors): updated author

--- contracts/Errors.sol
+++ contracts/Errors.sol
@@ -12,70 +12,11 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity 0.8.12;
+pragma solidity =0.8.12;
 
 /**
-* @dev Reverts if `condition` is false, with a revert reason containing `errorCode`. Only codes up to 999 are
-* supported.
-*/
-function _require(bool condition, uint256 errorCode) pure {
-    if (!condition) _revert(errorCode);
-}
-
-
-/**
- * @dev Reverts with a revert reason containing `errorCode`. Only codes up to 999 are supported.
+ * @author Forked from contracts developed by Balancer Labs and adapted by Swaap Labs
  */
-function _revert(uint256 errorCode) pure {
-    // We're going to dynamically create a revert uint256 based on the error code, with the following format:
-    // 'BAL#{errorCode}'
-    // where the code is left-padded with zeroes to three digits (so they range from 000 to 999).
-    //
-    // We don't have revert uint256s embedded in the contract to save bytecode size: it takes much less space to store a
-    // number (8 to 16 bits) than the individual uint256 characters.
-    //
-    // The dynamic uint256 creation algorithm that follows could be implemented in Solidity, but assembly allows for a
-    // much denser implementation, again saving bytecode size. Given this function unconditionally reverts, this is a
-    // safe place to rely on it without worrying about how its usage might affect e.g. memory contents.
-    assembly {
-        // First, we need to compute the ASCII representation of the error code. We assume that it is in the 0-999
-        // range, so we only need to convert three digits. To convert the digits to ASCII, we add 0x30, the value for
-        // the '0' character.
-
-        let units := add(mod(errorCode, 10), 0x30)
-
-        errorCode := div(errorCode, 10)
-        let tenths := add(mod(errorCode, 10), 0x30)
-
-        // With the individual characters, we can now construct the full uint256. The SWAAP# part is a known constant
-        // (0x535741415023): we simply shift this by 16 (to provide space for the 2 bytes of the error code), and add
-        // the characters to it, each shifted by a multiple of 8.
-        // The revert reason is then shifted left by 192 bits (256 minus the length of the uint256, 8 characters * 8
-        // bits per character = 64) to locate it in the most significant part of the 256 slot (the beginning of a byte
-        // array).
-
-        let revertReason := shl(192, add(0x5357414150230000, add(units, shl(8, tenths))))
-
-        // We can now encode the reason in memory, which can be safely overwritten as we're about to revert. The encoded
-        // message will have the following layout:
-        // [ revert reason identifier ] [ uint256 location offset ] [ uint256 length ] [ uint256 contents ]
-
-        // The Solidity revert reason identifier is 0x08c739a0, the function selector of the Error(uint256) function. We
-        // also write zeroes to the next 29 bytes of memory, but those are about to be overwritten.
-        mstore(0x0, 0x08c379a000000000000000000000000000000000000000000000000000000000)
-        // Next is the offset to the location of the uint256, which will be placed immediately after (20 bytes away).
-        mstore(0x04, 0x0000000000000000000000000000000000000000000000000000000000000020)
-        // The uint256 length is fixed: 8 characters.
-        mstore(0x24, 8)
-        // Finally, the uint256 itself is stored.
-        mstore(0x44, revertReason)
-
-        // Even if the uint256 is only 8 bytes long, we need to return a full 32 byte slot containing it. The length of
-        // the encoded message is therefore 4 + 32 + 32 + 32 = 100.
-        revert(0, 100)
-    }
-}
-
 library Err {
 
     uint256 internal constant REENTRY = 0;
@@ -121,7 +62,7 @@ library Err {
     uint256 internal constant POW_BASE_TOO_HIGH = 40;
     uint256 internal constant PRODUCT_OUT_OF_BOUNDS = 41;
     uint256 internal constant INVALID_EXPONENT = 42;
-    uint256 internal constant OUT_OF_BOUNDS = 43;    
+    uint256 internal constant OUT_OF_BOUNDS = 43;
     uint256 internal constant MAX_PRICE_UNPEG_RATIO = 44;
     uint256 internal constant PAUSE_WINDOW_EXCEEDED = 45;
     // uint256 internal constant MAX_PRICE_DELAY_SEC = 46;
@@ -137,4 +78,66 @@ library Err {
     uint256 internal constant MAX_MAX_PRICE_UNPEG_RATIO = 56;
     uint256 internal constant MAX_IN_RATIO = 57;
     uint256 internal constant MAX_OUT_RATIO = 58;
+}
+
+/**
+* @dev Reverts if `condition` is false, with a revert reason containing `errorCode`. Only codes up to 99 are
+* supported.
+*/
+function _require(bool condition, uint256 errorCode) pure {
+    if (!condition) _revert(errorCode);
+}
+
+
+/**
+ * @dev Reverts with a revert reason containing `errorCode`. Only codes up to 99 are supported.
+ */
+function _revert(uint256 errorCode) pure {
+    // We're going to dynamically create a revert uint256 based on the error code, with the following format:
+    // 'SWAAP#{errorCode}'
+    // where the code is left-padded with zeroes to three digits (so they range from 00 to 99).
+    //
+    // We don't have revert uint256s embedded in the contract to save bytecode size: it takes much less space to store a
+    // number (8 to 16 bits) than the individual uint256 characters.
+    //
+    // The dynamic uint256 creation algorithm that follows could be implemented in Solidity, but assembly allows for a
+    // much denser implementation, again saving bytecode size. Given this function unconditionally reverts, this is a
+    // safe place to rely on it without worrying about how its usage might affect e.g. memory contents.
+    assembly {
+        // First, we need to compute the ASCII representation of the error code. We assume that it is in the 0-99
+        // range, so we only need to convert three digits. To convert the digits to ASCII, we add 0x30, the value for
+        // the '0' character.
+
+        let units := add(mod(errorCode, 10), 0x30)
+
+        errorCode := div(errorCode, 10)
+        let tenths := add(mod(errorCode, 10), 0x30)
+
+        // With the individual characters, we can now construct the full uint256. The SWAAP# part is a known constant
+        // (0x535741415023): we simply shift this by 16 (to provide space for the 2 bytes of the error code), and add
+        // the characters to it, each shifted by a multiple of 8.
+        // The revert reason is then shifted left by 192 bits (256 minus the length of the uint256, 8 characters * 8
+        // bits per character = 64) to locate it in the most significant part of the 256 slot (the beginning of a byte
+        // array).
+
+        let revertReason := shl(192, add(0x5357414150230000, add(units, shl(8, tenths))))
+
+        // We can now encode the reason in memory, which can be safely overwritten as we're about to revert. The encoded
+        // message will have the following layout:
+        // [ revert reason identifier ] [ uint256 location offset ] [ uint256 length ] [ uint256 contents ]
+
+        // The Solidity revert reason identifier is 0x08c739a0, the function selector of the Error(uint256) function. We
+        // also write zeroes to the next 29 bytes of memory, but those are about to be overwritten.
+        mstore(0x0, 0x08c379a000000000000000000000000000000000000000000000000000000000)
+        // Next is the offset to the location of the uint256, which will be placed immediately after (20 bytes away).
+        mstore(0x04, 0x0000000000000000000000000000000000000000000000000000000000000020)
+        // The uint256 length is fixed: 8 characters.
+        mstore(0x24, 8)
+        // Finally, the uint256 itself is stored.
+        mstore(0x44, revertReason)
+
+        // Even if the uint256 is only 8 bytes long, we need to return a full 32 byte slot containing it. The length of
+        // the encoded message is therefore 4 + 32 + 32 + 32 = 100.
+        revert(0, 100)
+    }
 }
